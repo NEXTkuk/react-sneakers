b90aa7043b98d42c7bc6e67c1f668e9f3b60d351
Anton <nextkuk@bk.ru>
2023-04-21 11:11:40 +0300

    fix "Работа роутинга"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import "./index.scss";
@@ -10,8 +11,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
