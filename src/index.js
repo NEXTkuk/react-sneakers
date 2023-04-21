8f74d3733de0f28dae9c188ba446da61c34d5e51
Anton <nextkuk@bk.ru>
2023-04-21 11:19:22 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import "./index.scss";
@@ -11,8 +10,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
